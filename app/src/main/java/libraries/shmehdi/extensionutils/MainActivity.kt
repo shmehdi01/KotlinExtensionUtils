e0ab4392f689a5f9992dae43acac55ee80658392
Syed <syed@queuebuster.co>
2020-09-30 17:25:59 +0530

    first commit

---
+++ app/src/main/java/libraries/shmehdi/extensionutils/MainActivity.kt
@@ -0,0 +1,11 @@
+package libraries.shmehdi.extensionutils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
